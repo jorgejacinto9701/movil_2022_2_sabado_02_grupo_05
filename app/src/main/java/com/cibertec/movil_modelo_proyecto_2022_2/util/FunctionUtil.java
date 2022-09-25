ee2d985975f12da51e0f18874151d825d6addd89
jorge jacinto gutarra <jorgejacinto9701@gmail.com>
2022-09-24 21:51:30 -0500

    Update FunctionUtil.java

--- app/src/main/java/com/cibertec/movil_modelo_proyecto_2022_2/util/FunctionUtil.java
+++ app/src/main/java/com/cibertec/movil_modelo_proyecto_2022_2/util/FunctionUtil.java
@@ -19,7 +19,7 @@ public class FunctionUtil {
 	public static int ESTADO_INACTIVO = 0;
 
 	public static String getFechaActualStringDateTime() {
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSX");
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 		return sdf.format(new Date());
 	}
 
