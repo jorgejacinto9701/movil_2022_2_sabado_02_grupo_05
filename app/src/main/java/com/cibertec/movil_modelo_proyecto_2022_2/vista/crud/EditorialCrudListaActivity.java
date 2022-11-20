69db4b889fd8db5667bc0eb6676d4c0a10c9dd66
AmarilisAzul <i201721367@cibertec.edu.pe>
2022-11-19 20:51:38 -0500

    PC3-LECCA

--- app/src/main/java/com/cibertec/movil_modelo_proyecto_2022_2/vista/crud/EditorialCrudListaActivity.java
+++ app/src/main/java/com/cibertec/movil_modelo_proyecto_2022_2/vista/crud/EditorialCrudListaActivity.java
@@ -1,19 +1,36 @@
 package com.cibertec.movil_modelo_proyecto_2022_2.vista.crud;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.cibertec.movil_modelo_proyecto_2022_2.R;
+import com.cibertec.movil_modelo_proyecto_2022_2.adapter.AutorAdapter;
+import com.cibertec.movil_modelo_proyecto_2022_2.adapter.AutorCrudAdapter;
+import com.cibertec.movil_modelo_proyecto_2022_2.entity.Autor;
+import com.cibertec.movil_modelo_proyecto_2022_2.service.ServiceAutor;
+import com.cibertec.movil_modelo_proyecto_2022_2.service.ServiceGrado;
+import com.cibertec.movil_modelo_proyecto_2022_2.util.ConnectionRest;
 import com.cibertec.movil_modelo_proyecto_2022_2.util.NewAppCompatActivity;
+
+import java.util.ArrayList;
 
 
 public class EditorialCrudListaActivity extends NewAppCompatActivity {
 
-
-   
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_editorial_crud_lista);
+        setContentView(R.layout.activity_autor_crud_lista);
+
+
+
+
     }
 
 
